de515718d894f0818397b060d63ee3117ae0b027
Rituja Dixit <rituja.dixit@xebia.com>
2021-10-26 12:11:10 +0530

    adding Text Screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -11,6 +11,7 @@ const HomeScreen = ({ navigation }) => {
             <Button title="Go to Counter Demo" onPress={() => navigation.navigate('Counter')} />
             <Button title="Go to Color Demo" onPress={() => navigation.navigate('Color')} />
             <Button title="Go to Square Demo" onPress={() => navigation.navigate('Square')} />
+            <Button title="Go to Text Demo" onPress={() => navigation.navigate('Text')} />
         </View>
     );
 };
